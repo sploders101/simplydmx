e899726f5bfc44e8a95460777873de1659ac7bb6
Shaun Keys <mariobuddy@gmail.com>
2022-03-16 17:06:26 -0600

    Improve binary test

--- simplydmx_bin/src/main.rs
+++ simplydmx_bin/src/main.rs
@@ -1,23 +1,18 @@
 use serde_json::json;
+use serde_json::Value;
 
 use simplydmx_plugin_framework::{
 	Service,
-	service_docs,
 	interpolate_service,
 };
 
-use async_std::main;
-use std::pin::Pin;
-use std::sync::Arc;
-use serde_json::Value;
-use simplydmx_plugin_framework::services::internals::CallServiceError;
-use simplydmx_plugin_framework::services::internals::CallServiceJSONError;
-
 use std::{
+	sync::Arc,
 	boxed::Box,
-	any::Any,
-	future::Future,
+	time::Duration,
 };
+
+use async_std::task::sleep;
 
 struct Test ();
 #[interpolate_service(
@@ -27,15 +22,16 @@ struct Test ();
 	"Test service's description"
 )]
 impl TestService {
-	#[main(
+	#[service_main(
 		("Called From", "This indicates how the service was called."),
 		("Light", "This is the ID of the light that you would like to control"),
 		("New Value", "This is the value you want to assign to the light (0-65535)"),
 		("Misc Values", "These are some more miscellaneous values to show off the service framework", "some-stuff"),
 		("Formatted string", "This is a formatted string created from the inputs that were supplied"),
 	)]
-	pub fn call_internal(self, from: String, light_id: u32, value: Option::<u16>, values: Vec::<String>) -> String {
+	pub async fn call_internal(self, from: String, light_id: u32, value: Option::<u16>, values: Vec::<String>) -> String {
 		// Do stuff here
+		sleep(Duration::from_secs(1)).await;
 		return format!("From {}: Set light {:?} to {:?}. Here are some misc values: {:?}", from, light_id, value, values);
 	}
 }
@@ -49,7 +45,8 @@ async fn main() {
 	let service: Box<dyn Service> = Box::new(service);
 
 	// Call TestService as a generic Service trait
-	service.call(vec![
+	println!("Running with native values...");
+	let native_result = service.call(vec![
 		Box::new(String::from("Native Values")),
 		Box::new(15u32),
 		Box::new(Some(65535u16)),
@@ -60,8 +57,11 @@ async fn main() {
 			String::from("Value 4")
 		])
 	]).await.unwrap();
+	let native_result = native_result.downcast_ref::<String>().expect("Result should be a string");
+	println!("{}", native_result);
 
-	service.call_json(serde_json::from_str(r#"[
+	println!("Running with JSON values...");
+	let json_result = service.call_json(serde_json::from_str(r#"[
 		"JSON_________",
 		15,
 		65535,
@@ -72,6 +72,14 @@ async fn main() {
 			"Value 4"
 		]
 	]"#).unwrap()).await.unwrap();
+	if let Value::String(string) = json_result {
+		println!("{}", string);
+	} else {
+		panic!("Result should be a string");
+	}
+
+	// Give the user a chance to read the async service results
+	sleep(Duration::from_secs(1)).await;
 
 	// Print information supplied by TestService through the Service trait
 	println!("ID:          {}", service.get_id());
